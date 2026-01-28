a32ebcc5a00fd250f06933411003ae01ab82973f
Akshat Kumar <kumarakshat00007@gmail.com>
2026-01-29 00:56:37 +0530

    Updated files

---
+++ learning.js
@@ -0,0 +1,29 @@
+const Student = {
+    fullName : "Akshat Kumar",
+    age : 20,
+    cgpa : 8.6,
+    isPass : true,
+};
+    Student["age"] = Student["age"]+1;
+
+//product info
+
+const product = {
+    nameProduct : "Parker Jotter STander CT Ball Pen(Black)",
+    rating : 4,
+    price : 270,
+    offer : 5,
+};
+    product["rating"] = product["rating"] + 1;
+    console.log(product.rating)
+
+//social media
+
+const profile = {
+    handle : "Akshat",
+    posts : 195,
+    followers : 2100,
+    following : 4,
+    bio : "Abes Engg. | I,m wise enough to be fool"
+}
+    console.log(profile)
